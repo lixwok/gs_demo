22fbd21be077d139222b03a15af65650bd5fe756
lixwok <410230697@qq.com>
2015-04-22 00:13:13 +0800

    maven

--- src/test/java/cn/com/starit/xlab/test/JunitHelloWorld.java
+++ src/test/java/cn/com/starit/xlab/test/JunitHelloWorld.java
@@ -5,6 +5,16 @@ import static org.junit.Assert.assertEquals;
 /**
  * Created by Administrator on 2015/4/21.
  */
+//ctrl + alt + l 排版
+//双击alt显示各输出框
+//shift+esc最小化
+//ctrl + b实现
+//ctrl + shift + b 查看声明
+//ctrl + shift + 上下键 改变输出框大小
+//ctrl + w 选中标识符、行、多行
+//ctrl + z
+//ctrl + shift +z
+
 public class JunitHelloWorld {
     @Test
      public void helloWorldJunit(){
