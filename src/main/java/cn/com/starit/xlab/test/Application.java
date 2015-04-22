561fa369d2d64ced643a23cb1f06e101661f3512
lixwok <410230697@qq.com>
2015-04-22 18:05:11 +0800

    v1.0

--- src/main/java/cn/com/starit/xlab/test/Application.java
+++ src/main/java/cn/com/starit/xlab/test/Application.java
@@ -1,18 +1,142 @@
 package cn.com.starit.xlab.test;
+<<<<<<< Updated upstream
 
+=======
+import org.apache.http.HttpEntity;
+import org.apache.http.NameValuePair;
+import org.apache.http.ParseException;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.message.BasicNameValuePair;
+import org.apache.http.util.EntityUtils;
+>>>>>>> Stashed changes
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+<<<<<<< Updated upstream
+=======
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.util.ArrayList;
+import java.util.List;
+>>>>>>> Stashed changes
 
 /**
  * Created by Administrator on 2015/4/22.
  */
 @Configuration
 @ComponentScan
+<<<<<<< Updated upstream
 @EnableAutoConfiguration
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class);
+=======
+@Controller
+@EnableAutoConfiguration
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class,args);
+    }
+    @RequestMapping(value = "/hello",method = RequestMethod.GET)
+    public String hello(){
+        System.out.println("hello world");
+        get();
+        return "hello world";
+    }
+    /**
+     * post方式提交表单（模拟用户登录请求）
+     */
+    public void postForm() {
+        // 创建默认的httpClient实例.
+        CloseableHttpClient httpclient = HttpClients.createDefault();
+        // 创建httppost
+        HttpPost httppost = new HttpPost("http://60.174.249.204:8888/in/");
+        // 创建参数队列
+        List<NameValuePair> formparams = new ArrayList<NameValuePair>();
+        formparams.add(new BasicNameValuePair("username", "wei.changjie"));
+        formparams.add(new BasicNameValuePair("password", "weichangjie"));
+        UrlEncodedFormEntity uefEntity;
+        try {
+            uefEntity = new UrlEncodedFormEntity(formparams, "UTF-8");
+            httppost.setEntity(uefEntity);
+            System.out.println("executing request " + httppost.getURI());
+            CloseableHttpResponse response = httpclient.execute(httppost);
+            try {
+                HttpEntity entity = response.getEntity();
+                if (entity != null) {
+                    System.out.println("--------------------------------------");
+                    System.out.println("Response content: " + EntityUtils.toString(entity, "UTF-8"));
+                    System.out.println("--------------------------------------");
+                }
+            } finally {
+                response.close();
+            }
+        } catch (ClientProtocolException e) {
+            e.printStackTrace();
+        } catch (UnsupportedEncodingException e1) {
+            e1.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            // 关闭连接,释放资源
+            try {
+                httpclient.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+    /** * 发送 get请求
+     */
+    public void get() {
+        CloseableHttpClient httpclient = HttpClients.createDefault();
+        try {
+            // 创建httpget
+            HttpGet httpget = new HttpGet("http://60.174.249.204:8888/in/");
+            System.out.println("executing request " + httpget.getURI());
+            // 执行get请求.
+            CloseableHttpResponse response = httpclient.execute(httpget);
+            try {
+                // 获取响应实体
+                HttpEntity entity =  response.getEntity();
+                System.out.println("--------------------------------------");
+                // 打印响应状态
+                System.out.println(response.getStatusLine());
+                if (entity != null) {
+                    // 打印响应内容长度
+                    System.out.println("Response content length: " + entity.getContentLength());
+                    // 打印响应内容
+                    System.out.println("Response content: " + EntityUtils.toString(entity));
+                }
+                System.out.println("------------------------------------");
+            } finally {
+                response.close();
+            }
+        } catch (ClientProtocolException e) {
+            e.printStackTrace();
+        } catch (ParseException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            // 关闭连接,释放资源
+            try {
+                httpclient.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+>>>>>>> Stashed changes
     }
 }
